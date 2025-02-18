f222a681eb4933443df81b13a9b5e8b9ce05a318
lukasbygmal <lukasbygmal@gmail.com>
2025-02-18 19:39:09 +0100

    Added Action type and handle_action, changed parse_input to fit

---
+++ handle_action.h
@@ -0,0 +1,31 @@
+#ifndef HANDLE_ACTION_H
+#define HANDLE_ACTION_H
+
+#include <iostream>
+#include <optional>
+#include <string>
+
+struct Action
+{
+    std::string verb;
+    std::optional<std::string> direction;
+    std::optional<std::string> object;
+
+    Action(const std::string &v,
+           std::optional<std::string> d = std::nullopt,
+           std::optional<std::string> o = std::nullopt)
+        : verb(v), direction(d), object(o) {}
+
+    void print() const
+    {
+        std::cout << "Verb: " << verb << "\n";
+        if (direction)
+            std::cout << "Direction: " << *direction << "\n";
+        if (object)
+            std::cout << "Object: " << *object << "\n";
+    }
+};
+
+void handle_action(const Action &action);
+
+#endif // HANDLE_ACTION_H
--- parse_input.cpp
+++ parse_input.cpp
@@ -4,10 +4,11 @@
 #include <sstream>
 #include <algorithm>
 #include <cctype>
+#include "handle_action.h"
 
-bool parse_input(const std::string &input, std::tuple<bool, bool> &result)
+bool parse_input(const std::string &input, Action &action)
 {
-    std::vector<std::string> valid_verbs = {"go", "rest", "investigate", "search", "open", "pray",};
+    std::vector<std::string> valid_verbs = {"go", "rest", "investigate", "search", "open", "pray"};
     std::vector<std::string> valid_directions = {"left", "right", "middle"};
     std::vector<std::string> valid_objects = {"chest", "engraving", "corpse"};
 
@@ -21,69 +22,31 @@ bool parse_input(const std::string &input, std::tuple<bool, bool> &result)
         words.push_back(word);
     }
 
-    if (words.empty())
+    if (words.empty() || std::find(valid_verbs.begin(), valid_verbs.end(), words[0]) == valid_verbs.end())
     {
-        result = std::make_tuple(false, true);
         return false;
     }
 
-    if (std::find(valid_verbs.begin(), valid_verbs.end(), words[0]) == valid_verbs.end())
+    if (words[0] == "go" && words.size() == 2 && std::find(valid_directions.begin(), valid_directions.end(), words[1]) != valid_directions.end())
     {
-        result = std::make_tuple(false, true);
-        return false;
+        action = Action(words[0], words[1]);
+        return true;
+    }
+    else if (words[0] == "investigate" && words.size() == 2 && std::find(valid_objects.begin(), valid_objects.end(), words[1]) != valid_objects.end())
+    {
+        action = Action(words[0], std::nullopt, words[1]);
+        return true;
+    }
+    else if (words[0] == "open" && words.size() == 2 && words[1] == "chest")
+    {
+        action = Action(words[0], std::nullopt, words[1]);
+        return true;
+    }
+    else if ((words[0] == "search" || words[0] == "rest" || words[0] == "pray") && words.size() == 1)
+    {
+        action = Action(words[0]);
+        return true;
     }
 
-    // Looks ugly with so many if statements but i suppose this is what "AI" is :P
-
-    if (words[0] == "go")
-    {
-        if (words.size() != 2 || std::find(valid_directions.begin(), valid_directions.end(), words[1]) == valid_directions.end())
-        {
-            result = std::make_tuple(false, true);
-            return false;
-        }
-    }
-    else if (words[0] == "investigate")
-    {
-        if (words.size() != 2 || std::find(valid_objects.begin(), valid_objects.end(), words[1]) == valid_objects.end())
-        {
-            result = std::make_tuple(false, true);
-            return false;
-        }
-    }
-    else if (words[0] == "open")
-    {
-        if (words.size() != 2 || words[1] != "chest")
-        {
-            result = std::make_tuple(false, true);
-            return false;
-        }
-    }
-    else if (words[0] == "search")
-    {
-        if (words.size() != 1)
-        {
-            result = std::make_tuple(false, true);
-            return false;
-        }
-    }
-    else if (words[0] == "rest")
-    {
-        if (words.size() != 1)
-        {
-            result = std::make_tuple(false, true);
-            return false;
-        }
-    }
-    else if (words[0] == "pray")
-    {
-        if (words.size() != 1)
-        {
-            result = std::make_tuple(false, true);
-            return false;
-        }
-    }
-
-    result = std::make_tuple(true, true);
-    return true;
+    return false;
 }
