1c794288fea93801a61e92437c52d16465f4dc92
lukasbygmal <lukasbygmal@gmail.com>
2025-02-22 00:52:40 +0100

    Changed main to use game and gameui class

--- handle_action.h
+++ handle_action.h
@@ -1,32 +1,9 @@
 #ifndef HANDLE_ACTION_H
 #define HANDLE_ACTION_H
 
-#include <iostream>
-#include <optional>
-#include <string>
+#include "action.h"
 #include "dungeon.h"
-
-struct Action
-{
-    std::string verb;
-    std::optional<std::string> direction;
-    std::optional<std::string> object;
-
-    Action(const std::string &v,
-           std::optional<std::string> d = std::nullopt,
-           std::optional<std::string> o = std::nullopt)
-        : verb(v), direction(d), object(o) {}
-
-    void print() const
-    {
-        std::cout << "Verb: " << verb << "\n";
-        if (direction)
-            std::cout << "Direction: " << *direction << "\n";
-        if (object)
-            std::cout << "Object: " << *object << "\n";
-    }
-};
 
 void handle_action(const Action &action, Dungeon &dungeon);
 
-#endif // HANDLE_ACTION_H
+#endif
