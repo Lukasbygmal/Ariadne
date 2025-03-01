e8ef336c1325b411cb7cd487585e8900d843b381
lukasbygmal <lukasbygmal@gmail.com>
2025-03-01 11:48:44 +0100

    Re-merged game and game_ui classes

--- handle_action.cpp
+++ handle_action.cpp
@@ -3,66 +3,76 @@
 
 void handle_action(const Action &action, Game &game)
 {
-    if (action.verb == "enter" && action.object)
+    switch (game.getMode())
     {
-        if (*action.object == "easy")
+    case GameMode::MENU:
+        if (action.verb == "enter" && action.object)
         {
-            game.enterDungeon(4, 1);
+            if (*action.object == "easy")
+            {
+                game.enterDungeon(4, 1);
+            }
+            else if (*action.object == "medium")
+            {
+                game.enterDungeon(6, 3);
+            }
+            else if (*action.object == "hard")
+            {
+                game.enterDungeon(8, 5);
+            }
         }
-        else if (*action.object == "medium")
-        {
-            game.enterDungeon(6, 3);
-        }
-        else if (*action.object == "hard")
-        {
-            game.enterDungeon(8, 5);
-        }
-    }
-    else if (action.verb == "go" && action.direction)
-    {
-        std::cout << "Go(" << *action.direction << ")" << std::endl;
+        break;
 
-        Dungeon &dungeon = game.getDungeon();
-        if (*action.direction == "east")
+    case GameMode::DUNGEON:
+        if (action.verb == "go" && action.direction)
         {
-            dungeon.goEast();
+            std::cout << "Go(" << *action.direction << ")" << std::endl;
+            Dungeon &dungeon = game.getDungeon();
+            if (*action.direction == "east")
+            {
+                dungeon.goEast();
+            }
+            else if (*action.direction == "west")
+            {
+                dungeon.goWest();
+            }
+            else if (*action.direction == "north")
+            {
+                dungeon.goNorth();
+            }
+            else if (*action.direction == "south")
+            {
+                dungeon.goSouth();
+            }
+            game.checkRoomHazards();
         }
-        else if (*action.direction == "west")
+        else if (action.verb == "investigate" && action.object)
         {
-            dungeon.goWest();
+            std::cout << "Investigate(" << *action.object << ")" << std::endl;
         }
-        else if (*action.direction == "north")
+        else if (action.verb == "open" && action.object)
         {
-            dungeon.goNorth();
+            std::cout << "Open(" << *action.object << ")" << std::endl;
         }
-        else if (*action.direction == "south")
+        else if (action.verb == "search")
         {
-            dungeon.goSouth();
+            std::cout << "Search()" << std::endl;
         }
-        game.checkRoomHazards();
-    }
-    else if (action.verb == "investigate" && action.object)
-    {
-        std::cout << "Investigate(" << *action.object << ")" << std::endl;
-    }
-    else if (action.verb == "open" && action.object)
-    {
-        std::cout << "Open(" << *action.object << ")" << std::endl;
-    }
-    else if (action.verb == "search")
-    {
-        std::cout << "Search()" << std::endl;
-    }
-    else if (action.verb == "rest")
-    {
-        std::cout << "Rest()" << std::endl;
-    }
-    else if (action.verb == "pray")
-    {
-        std::cout << "Pray()" << std::endl;
-    }
-    else
-    {
-        std::cout << "Invalid action." << std::endl;
+        else if (action.verb == "rest")
+        {
+            std::cout << "Rest()" << std::endl;
+        }
+        else if (action.verb == "pray")
+        {
+            std::cout << "Pray()" << std::endl;
+        }
+        else
+        {
+            std::cout << "Invalid action." << std::endl;
+        }
+        break;
+
+    case GameMode::BATTLE:
+        break;
     }
 }
