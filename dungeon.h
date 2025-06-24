0ad184b946baf00fd4ff5cab8c54b1b244694499
lukasbygmal <lukasbygmal@gmail.com>
2025-06-24 17:07:10 +0200

    Dungeon added words and word_length, displayed in upper case, cleaned movement

--- dungeon.h
+++ dungeon.h
@@ -8,6 +8,8 @@
 struct DungeonConfig
 {
     int size;
+    int words;       // needs to be >4, since defend words are -2
+    int word_length; // needs to be >2, since defend words are -1
     std::vector<int> monster_pool;
     int boss_monster_type;
     int base_difficulty;
@@ -22,12 +24,16 @@ private:
     std::string name;
     std::string difficulty_string;
     int size;
+    int words;       // needs to be >4, since defend words are -2
+    int word_length; // needs to be >2, since defend words are -1
     int difficulty;
     int current_x;
     int current_y;
     Room *current_room;
     int boss_room_x; // not sure i need this
     int boss_room_y; // not sure i need this
+    bool move(int dx, int dy);
+    std::string capitalizeFirstLetter(const std::string &input) const;
 
 public:
     Dungeon(std::string dungeon_name, std::string string_difficulty);
@@ -41,6 +47,8 @@ public:
     int getCurrentX() const;
     int getCurrentY() const;
     bool isBossRoom() const; // not sure i need this
+    int getWordLength() const { return word_length; }
+    int getWords() const { return words; }
     std::string to_string() const;
 };
 
