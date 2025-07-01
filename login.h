c4248b39705337582d14699cac27bb52dfd09d46
lukasbygmal <lukasbygmal@gmail.com>
2025-07-01 19:05:22 +0200

    Changed so paste is in client, cleaned up login

--- login.h
+++ login.h
@@ -1,19 +1,52 @@
-#pragma once
+#ifndef LOGIN_H
+#define LOGIN_H
+
 #include <SFML/Graphics.hpp>
 #include <string>
 
 class Login
 {
 public:
-    Login(sf::RenderWindow &window);
-    // Returns true if login was successful, sets user_id
+    Login(sf::RenderWindow &win);
     bool run(int &user_id);
+    void draw();
+    void handleEvent(const sf::Event &event, bool &tryLogin, bool &submitCode);
 
 private:
     sf::RenderWindow &window;
     sf::Font font;
-    sf::Text titleText, loginButton, errorText;
+    
+    sf::Text titleText;
     sf::RectangleShape buttonBox;
-    void draw();
-    void handleEvent(const sf::Event &event, bool &tryLogin);
+    sf::Text loginButton;
+    sf::RectangleShape inputBox;
+    sf::Text inputText;
+    sf::Text promptText;
+    sf::RectangleShape submitButton;
+    sf::Text submitButtonText;
+    sf::Text errorText;
+    
+    std::string inputString;
+    bool waitingForCode;
+    bool inputActive;
+
+    void drawInputElements();
+
+    void handleEvents(bool &tryLogin, bool &submitCode);
+    void handleMouseClick(const sf::Event::MouseButtonEvent &mouse, bool &tryLogin, bool &submitCode);
+    void handleKeyPress(const sf::Event::KeyEvent &key, bool &tryLogin, bool &submitCode);
+    void handleTextInput(const sf::Event::TextEvent &text);
+
+    void setInputActive(bool active);
+    void pasteFromClipboard();
+    void handleBackspace();
+
+    void initiateLogin();
+    bool processCodeSubmission(int &user_id);
+
+    void clearInput();
+    void clearError();
+    void setError(const std::string &message);
 };
+
+#endif
